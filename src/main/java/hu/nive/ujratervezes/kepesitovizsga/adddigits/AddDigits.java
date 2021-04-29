00fc403e7cf0574aa582be3ca61b522986ffe657
levosoft <lukacs.levente.geza@gmail.com>
2021-04-29 11:29:55 +0200

    addigits

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/adddigits/AddDigits.java
@@ -1,4 +1,29 @@
 package hu.nive.ujratervezes.kepesitovizsga.adddigits;
 
 public class AddDigits {
+
+
+    public int addDigits(String input) {
+
+        int digitSum = 0;
+
+        if (!(input.equals("")) || !(input.equals(null)) || !(input.isEmpty())){
+
+            for (char c: input.toCharArray()) {
+                if (Character.isDigit(c)){
+
+                    int i = c - '0';
+                    digitSum = digitSum + i;
+
+                }
+            }
+            return digitSum;
+
+        }else {
+            return -1;
+        }
+
+    }
+
+
 }
